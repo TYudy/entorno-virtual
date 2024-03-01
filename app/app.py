19d8013a2bf00024ab026c363522619fc4c54906
Yudy <cn.t0604@gmail.com>
2024-03-01 09:22:41 -0500

    registro db

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import *
+from flask import Flask , render_template,request,redirect,url_for
 import mysql.connector
 #Instancia
 app = Flask (__name__)
@@ -6,7 +6,7 @@ db = mysql.connector.connect(
     host = "localhost",
     user = "root",
     password="",
-    database = "Agenda"
+    database = "agenda"
 
 )
 
@@ -18,26 +18,29 @@ def index():
 
 
 
-@app.route('/registrar', methods=['POST'])
+@app.route('/registrar', methods=['GET','POST'])
 def registrar_usuario():
-    Nombres = request.form['p_Nombre'],
-    Apellidos = request.form['p_Apellido'],
-    Email = request.form['p_Email'],
-    Direccion = request.form['p_Dirección'],
-    Telefono = request.form['p_Telefono'],
-    Usuario= request.form['p_Usuario'],
-    Contrasena = request.form['p_Contrasena'],
+    if request.method == 'POST':
+        Nombres = request.form.get('p_Nombre')
+        Apellidos = request.form.get('p_Apellido')
+        Email = request.form.get('p_Email')
+        Direccion = request.form.get('p_Direccion')
+        Telefono = request.form.get('p_Telefono')
+        Usuario= request.form.get('p_Usuario')
+        Contrasena = request.form.get('p_Contrasena')
  #insertar datos a la tabla persona
     
-    cursor.execute("INSERT INTO Persona(p_Nombre, p_Apellido, p_Email, p_Dirección, p_Telefono, p_Usuario, p_Contraseña)VALUES(%s,%s,%s,%s,%s,%s,%s)",(Nombres, Apellidos, Email, Direccion, Telefono, Usuario, Contrasena))
-    db.commit()
-   
-    return redirect(url_for('/registrar'))
+        cursor.execute("INSERT INTO persona(P_Nombre, p_Apellido, p_Email, p_Direccion, p_Telefono, p_Usuario, p_Contraseña)VALUES(%s,%s,%s,%s,%s,%s,%s)",(Nombres, Apellidos, Email, Direccion, Telefono, Usuario, Contrasena))
+        db.commit()
+    
+        return redirect(url_for('registrar_usuario'))
+        return render_template
+    return render_template("Registrar.html")
 
    
 
 
 if __name__ == '__main__':
     app.add_url_rule('/', view_func=index)
-    app.run(debug = True, port=5005)
+    app.run(debug = True, port=5000)
 #Rutas
